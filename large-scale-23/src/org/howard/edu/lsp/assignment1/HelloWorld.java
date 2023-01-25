61547f2040dcb14415e603a6326c9071f37ee7dd
Brandon Clarke <brandon.clarke1430@gmail.com>
2023-01-24 23:34:14 -0500

    "Added name comment and deleted spaces"

--- large-scale-23/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ large-scale-23/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,10 +1,13 @@
+/**
+ * Name: Brandon Clarke
+ * */
+
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// Printing Hello World - Brandon Clarke
-		System.out.println("Hello World!!!");
+		System.out.println("Hello World!!");
 	}
 
 }
