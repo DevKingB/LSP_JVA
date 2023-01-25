1268a4aa5a77d8fb4e5b4f0edb99f3d123b484c5
Brandon Clarke <brandon.clarke1430@gmail.com>
2023-01-24 23:31:41 -0500

    "Updated HelloWorld.java file"

---
+++ large-scale-23/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Printing Hello World - Brandon Clarke
+		System.out.println("Hello World!!!");
+	}
+
+}
