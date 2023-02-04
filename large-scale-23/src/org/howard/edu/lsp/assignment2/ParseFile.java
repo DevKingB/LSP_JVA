19e57f5d691121744b32ecac489e76de10dc1978
Brandon Clarke <brandon.clarke1430@gmail.com>
2023-02-04 10:07:16 -0500

    Added sample text to Word.txt and updated ParserFile for reading and counting words.

--- large-scale-23/src/org/howard/edu/lsp/assignment2/ParseFile.java
+++ large-scale-23/src/org/howard/edu/lsp/assignment2/ParseFile.java
@@ -1,5 +1,38 @@
 package org.howard.edu.lsp.assignment2;
+import java.util.*;
+import java.io.FileNotFoundException;
+
 
 public class ParseFile {
-
+	public static void main(String[] args) {
+		//instantiating the File Reader class and Hash Map
+		FileReader fileReader = new FileReader();
+		HashMap<String, Integer> myHashMap = new HashMap<String, Integer>();
+		
+		try {
+			//converting file content into a string
+			String fileToString = fileReader.readToString("main/resources/word.txt");
+			
+			//solves for punctuation and splits by spaces
+			String[] splitWords = fileToString.replaceAll("[^a-zA-z ]", "").toLowerCase().split(" ");
+			
+			//loops through and updates the dictionary based on conditionals
+			for(int i=0; i < splitWords.length; i++) {
+				if(splitWords[i].length()>3) {
+					if(myHashMap.get(splitWords[i])== null) {
+						myHashMap.put(splitWords[i], 1);
+					} else {
+						myHashMap.put(splitWords[i], myHashMap.get(splitWords[i]) + 1);
+					}
+				}
+			}
+			
+			//prints the keys and values in the hash map
+			for (String i:myHashMap.keySet()) {
+				System.out.println(i + " " + myHashMap.get(i));
+			}
+		} catch (FileNotFoundException e){
+			System.out.println(e);
+		};
+	}
 }
